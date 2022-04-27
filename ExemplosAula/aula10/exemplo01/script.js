b5a8c55a7df1041ab4fc34ec724e39ff424e3ccd
unknown <nicolastheodoro@gmail.com>
2022-04-26 21:04:47 -0300

    subindo exemplo aula10

--- ExemplosAula/aula10/exemplo01/script.js
+++ ExemplosAula/aula10/exemplo01/script.js
@@ -1,1 +1,23 @@
+function carregar() {
+  let mensagem = document.getElementById("container-msg");
+  let imagem = document.getElementById("img");
 
+  let data = new Date();
+  // let hora = data.getHours();
+  let hora = 19;
+  mensagem.innerHTML = `Agora são ${hora} horas.`;
+
+  if (hora >= 0 && hora < 12) {
+    // Manhã
+    imagem.src = "./assets/manha.jpg";
+    document.body.style.background = "#F7EAD8";
+  } else if (hora >= 12 && hora <= 18) {
+    // Tarde
+    imagem.src = "./assets/tarde.jpg";
+    document.body.style.background = "#AF773A";
+  } else {
+    // Noite
+    imagem.src = "./assets/noite.jpg";
+    document.body.style.background = "#15272B";
+  }
+}
